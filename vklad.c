4931f654c53222c30ffadfeafc05210cf5e246bf
sergo666 <serrrfff@gmail.com>
2017-02-15 21:29:03 +0700

    fix3

--- vklad.c
+++ vklad.c
@@ -1,34 +1,69 @@
 #include <stdio.h>
 
+
 int main()
 {
 	int srok,i;
 	float vklad;
 
-	printf("vvedi srok u summu vklada\n");
-	scanf("%d %f",&srok,&vklad);
+	printf("Срок:\n");        
+	scanf("%d",&srok);
+	printf("Сумма вклада:\n");
+	scanf("%f",&vklad);
 
-	if(vklad<=100000)
-		if(srok<=30)
-			vklad=vklad*0.9;
-		if((srok>=31)&&(srok<=120))
-			vklad=vklad*1.02;
-		if((srok>=121)&&(srok<=240))
-			vklad=vklad*1.06;
-		if((srok>=241)&&(srok<=365))
-			vklad=vklad*1.12;
+    if((srok>365)||(srok<0)||(vklad<10000)) //проверка входных данных
+        perror("incorrect input");
+    
 
+    if(vklad<=100000)
+    {
+        if(srok<=30)
+        {
+	    vklad=vklad*0.9;
+        }
+        else
+        
+            if((srok>=31)&&(srok<=120)){
+                vklad=vklad*1.02;
+        }
+        else
+        
+            if((srok>=121)&&(srok<=240)){
+                vklad=vklad*1.06;
+        }
+        else
+        
+            if((srok>=241)&&(srok<=365)){	
+                vklad=vklad*1.12;
+        }
+    }
+    else
+    {
 	if(vklad>100000)
-		if(srok<=30)
-			vklad=vklad*0.9;
-		if((srok>=31)&&(srok<=120))
-			vklad=vklad*1.03;
-		if((srok>=121)&&(srok<=240))
-			vklad=vklad*1.08;
-		if((srok>=241)&&(srok<=365))
-			vklad=vklad*1.15;
-	
-	printf("%f",vklad);
+        {
+            if(srok<=30)
+            {
+	        vklad=vklad*0.9;
+            }
+            else
+            
+                if((srok>=31)&&(srok<=120)){
+                    vklad=vklad*1.03;
+            }
+            else
+            
+                if((srok>=121)&&(srok<=240)){
+                    vklad=vklad*1.08;
+            }
+            else
+            
+                if((srok>=241)&&(srok<=365)){	
+                    vklad=vklad*1.15;
+            }
+        }
+    }
+
+	printf("%g\n",vklad);
 	
 	return 0;
 }
